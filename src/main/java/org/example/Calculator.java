1bba16f84ed0f488f17fe92d1b25abae17fd8654
Chlebab <miksob@gmail.com>
2024-06-07 20:56:59 +0100

    done

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Calculator {
+    public int add(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+    public double add(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Calculator().add(1,5));
+        System.out.println(new Calculator().add(1.5123,5534215.1535));
     }
 }
