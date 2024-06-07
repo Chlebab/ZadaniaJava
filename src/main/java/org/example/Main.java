fdae8950d082dd16b3c89ba3efc48a3a8d1a255a
Chlebab <miksob@gmail.com>
2024-06-07 20:52:14 +0100

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Main().sub(null,2));
+        System.out.println(new Main().sub(1,null));
+    }
+    public int sub(Integer operand1, Integer operand2) {
+        try {
+            return operand1 - operand2;
+        } catch (NullPointerException e) {
+            if (operand1 == null) operand1 = 0;
+            if (operand2 == null) operand2 = 0;
+            return operand1 - operand2;
+        }
     }
 }
