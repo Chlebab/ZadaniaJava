d98eebdcb77183febae3a14fb92e9b8cf7a32158
Chlebab <miksob@gmail.com>
2024-06-07 20:40:16 +0100

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.klasy.Calculator;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(new Calculator().add(1,2));
+        System.out.println(new Calculator().add(1,0));
+        System.out.println(Calculator.multiply(-2,5));
+        System.out.println(Calculator.multiply(2,3));
     }
 }
---
+++ src/main/java/org/example/klasy/Calculator.java
@@ -0,0 +1,12 @@
+package org.example.klasy;
+
+public class Calculator {
+
+    public int add(int operand1, int operand2) {
+        return operand1 + operand2;
+    }
+    public static int multiply(int operand1, int operand2) {
+        return operand1 * operand2;
+    }
+
+}
